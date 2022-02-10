0b58ffa84f4d50f2abe32583a5fe388d99193a4d
E7ite <z7z.e7ite.z7z@gmail.com>
2022-02-10 19:38:38 +0000

    Added GTK3 example to confirm functionality of tooling

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,12 @@
 #include <gtkmm.h>
-#include <iostream>
 
-int main(int argc, const char **argv) 
+int main(int argc, char *argv[])
 {
-    std::cout << "Hello\n";
+    Glib::RefPtr<Gtk::Application> app = Gtk::Application::create(argc, argv,
+                                                                  "org.gtkmm.examples.base");
+
+    Gtk::Window window;
+    window.set_default_size(200, 200);
+
+    return app->run(window);
 }
