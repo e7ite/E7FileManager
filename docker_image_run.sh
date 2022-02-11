800242c255e944f6b3da3933603cd9d509a9cbdc
E7ite <z7z.e7ite.z7z@gmail.com>
2022-02-11 05:19:46 +0000

    Add name to Docker container to MacOS startup script

--- docker_image_run.sh
+++ docker_image_run.sh
@@ -9,7 +9,7 @@ docker build -t filemanagerdev /Users/david/Documents/Classes/CSCI490/E7FileMana
 # Not working due /usr/local/bin/devcontainer script not launching not finding exectuable because of bad configuration
 #devcontainer open /Users/david/Documents/Classes/CSCI490/E7FileManager/ 
 
-docker run -itd -e DISPLAY=$IP:0 \
+docker run -itd --name filemanagerdevinst -e DISPLAY=$IP:0 \
     -v /tmp/.X11-unix/:/tmp/.X11-unix/ \
     -v /Users/david/.ssh/:/root/.ssh \
     -v /Users/david/Documents/Classes/CSCI490/E7FileManager/:/project/ \
