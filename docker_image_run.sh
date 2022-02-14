24fabc0a06b1592390b133202ca6f74654015106
E7ite <z7z.e7ite.z7z@gmail.com>
2022-02-13 19:58:00 -0800

    Make MacOS Docker container creation script start from any path.

--- docker_image_run.sh
+++ docker_image_run.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
+#
+# MacOS script to build Docker image and run a Docker container with X11
+# forwarding with XQuartz and git credentials. 
+#
+# Expects XQuartz to be installed
+#
+# Usage:
+# ./docker_image_run.sh
 
-export IP=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
+greadlink_path="$(which greadlink)"
+if [ -z "$greadlink_path" ]
+then
+    echo "greadlink needs to be installed. You can install via `brew install coreutils`"
+    exit 1
+fi
+
+IP=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
+
+script_path=$(dirname "$(greadlink -f "$0")")
 
 xhost + $IP
 
-docker build -t filemanagerdev /Users/david/Documents/Classes/CSCI490/E7FileManager
-
-# Not working due /usr/local/bin/devcontainer script not launching not finding exectuable because of bad configuration
-#devcontainer open /Users/david/Documents/Classes/CSCI490/E7FileManager/ 
-
+docker build -t filemanagerdev $script_path
 docker run -itd --name filemanagerdevinst -e DISPLAY=$IP:0 \
     -v /tmp/.X11-unix/:/tmp/.X11-unix/ \
-    -v /Users/david/.ssh/:/root/.ssh \
-    -v /Users/david/Documents/Classes/CSCI490/E7FileManager/:/project/ \
+    -v "$HOME/.ssh/:/root/.ssh" \
+    -v "$script_path/:/project/" \
     filemanagerdev /bin/bash
